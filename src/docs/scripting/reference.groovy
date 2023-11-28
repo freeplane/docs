a89ae3dca4f7aa9c89af2d865f6a1859f8695ca1
euu <77707706+euu2021@users.noreply.github.com>
2023-11-28 14:42:10 -0300

    Update reference.groovy

--- src/docs/scripting/reference.groovy
+++ src/docs/scripting/reference.groovy
@@ -44,3 +44,50 @@ node.attributes.empty
 
 node.attributes.size()
 
+/*
+  Attributes
+*/
+
+// Adds an attribute with key "aaa" and value "xxx", no matter if an attribute with the given name already exists.
+
+node.attributes.add("aaa", "xxx")
+
+// Adds an attribute with key "aaa", value "8", with formatting pattern "0.0", no matter if an attribute with the given name already exists.
+
+node.attributes.add("aaa", "8", "0.0")
+
+// Removes all attributes of the selected node.
+
+node.attributes.clear()
+
+// Optimizes widths of attribute view columns according to contents.
+
+node.attributes.optimizeWidths()
+
+// Removes the attribute at the index 1.
+
+node.attributes.remove(1)
+
+// Removes all attributes with name "aaa".
+
+node.attributes.removeAll("aaa")
+
+// Sets "xxx" as the value of the attribute at index 1.
+
+node.attributes.set(1,"xxx")
+
+// Sets "aaa" as the key, and "xxx" as the value of the attribute at index 1.
+
+node.attributes.set(1, "aaa", "xxx")
+
+// Sets "aaa" as the key, "8" as the value, and "0.0" as the format pattern of the attribute at index 1.
+
+node.attributes.set(1, "aaa", "8", "0.0")
+
+// Adds an attribute with key "aaa" and value "xxx" if there is no attribute with the given key or changes the value of the first attribute with the given key.
+
+node.attributes.set("aaa", "xxx")
+
+// Sets "0.0" as the format pattern to the attribute at index 1.
+
+node.attributes.setFormat(1, "0.0")
